24107b77511f2d591c5a330e048931e036318f3b
Earl Hans <earlhansg@gmail.com>
2025-09-23 07:17:02 +0800

    setup mock data for books

---
+++ src/data/books.ts
@@ -0,0 +1,12 @@
+import type { Book } from "../types/NetworkRequest";
+
+export const BOOKS_DATA: Book[] = [
+  { title: "Fear and Loathing in Las Vegas", author: "Hunter S. Thompson", rating: 4.5 },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", rating: 4.0 },
+  { title: "Moby Dick", author: "Herman Melville", rating: 3.5 },
+  { title: "Pride and Prejudice", author: "Jane Austen", rating: 4.5 },
+  { title: "Dune", author: "Frank Herbert", rating: 5.0 },
+  { title: "Flowers in the Attic", author: "Virginia Andrews", rating: 1.0 },
+  { title: "Moon People", author: "Dale M. Courtney", rating: 0.0 },
+  { title: "The Da Vinci Code", author: "Dan Brown", rating: 2.5 },
+];
